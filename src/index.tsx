5db158ed8d2f270a0d7d5d5f3784e0562e72dd04
Dondomie Dungca <ddungca@isbx.com>
2022-08-26 11:09:52 +0800

    test 2

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-console.log('test');
+console.log('test 2');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
